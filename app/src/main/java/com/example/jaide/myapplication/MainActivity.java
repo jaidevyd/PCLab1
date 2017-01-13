20101240c7fa87b0b88d7368a313142e4e6e1d89
jaidevyd <jaidevyd@gmail.com>
2017-01-12 21:12:47 -0500

    Lab1 commit

--- app/src/main/java/com/example/jaide/myapplication/MainActivity.java
+++ app/src/main/java/com/example/jaide/myapplication/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.jaide.myapplication;
 
+import android.app.Activity;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    Button b;
+    Button t;
+    Button f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b = (Button) findViewById(R.id.button);
+        t = (Button) findViewById(R.id.buttonTrue);
+        f = (Button) findViewById(R.id.buttonFalse);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Hello World", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        t.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "True", Toast.LENGTH_LONG).show();
+                t.setTextColor(Color.BLUE);
+            }
+        });
+
+        f.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "False", Toast.LENGTH_LONG).show();
+                f.setTextColor(Color.BLUE);
+            }
+        });
     }
 }
